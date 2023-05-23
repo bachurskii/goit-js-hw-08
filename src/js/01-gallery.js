dd23b4494dd7f78c46bc927755fd5aa9a2b23e66
Georgen1 <123319264+bachurskii@users.noreply.github.com>
2023-05-23 14:43:26 +0300

    new update
    my new update to my  repositorii

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,59 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector('.gallery');
+
+const markUP = galleryItems
+  .map(
+    ({ original, description, preview }) =>
+      `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </li>`
+  )
+  .join('');
+
+gallery.insertAdjacentHTML('beforeend', markUP);
+
+gallery.addEventListener('click', elem => {
+  elem.preventDefault();
+  if (elem.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const choicesImages = elem.target.getAttribute('data-source');
+
+  const options = basicLightbox.create(
+    `<img src ="${choicesImages}" width ="800" height="600">`,
+    {
+      onShow: () => {
+        document.addEventListener('keydown', escapeClick);
+      },
+      onclose: () => {
+        document.removeEventListener('keydown', escapeClick);
+      },
+    }
+  );
+  options.show();
+
+  document.addEventListener('keydown');
+  function escapeClick(event) {
+    if (event.key === 'Escape') {
+      options.close();
+    }
+  }
+});
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import { throttle } from 'lodash';
 
+const iframe = document.querySelector('iframe');
+const videoPlayer = new Player(iframe);
+
+videoPlayer.on(
+  'timeupdate',
+  throttle(e => {
+    localStorage.setItem('videoplayer-current-time', e.seconds);
+  }, 1000)
+);
+videoPlayer
+  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .catch(function (eror) {
+    console.eror(eror);
+  });
